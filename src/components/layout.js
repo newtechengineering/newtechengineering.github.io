94ca9e114f2c976702133a52a71827e453b999b2
Archit Kaushik <asarchit65@gmail.com>
2019-02-08 17:43:27 +0530

    styled

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,11 +25,15 @@ const Layout = ({ children }) => (
           {children}
           <footer>
             <div class="container">
-              <div class="row">
+              <div class="row mb-flex">
                 <div class="col-md-4">
-                  <div class="widget">
+                  <div class="widget" style={{ height: '100%' }}>
                     <iframe
-                      src="https://www.google.com/maps/d/embed?mid=1qKbE-cmcWO2xzrO53d5c012IrUUndvr6"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d892.6899992411768!2d80.28198692210107!3d26.495671368928583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399c38269f89ed2d%3A0x1479bff457dcbaaf!2sLakhanpur%2C+Lakhanpur+Housing+Society%2C+Khyora%2C+Kanpur%2C+Uttar+Pradesh+208024!5e0!3m2!1sen!2sin!4v1549627375793"
+                      allowfullscreen
+                      frameborder="0"
+                      width="100"
+                      height="250"
                       style={{
                         width: '100%',
                         height: '100%',
