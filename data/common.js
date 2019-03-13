71b7e4cbaad9106e98d1001953305eacf69d734f
Archit Kaushik <asarchit65@gmail.com>
2019-03-13 19:17:45 +0530

    Added email and fixed zoom effect on box-item

--- data/common.js
+++ data/common.js
@@ -6,7 +6,7 @@ let common = {
         'Plot No.- 522, Lakhanpur Housing Society, Vikas Nagar, Kanpur (UP), India',
     },
   ],
-  email: [{ value: 'anuragshukla87@yahoo.in' }],
+  email: [{ value: 'contact@newtechpackaging.in' },{ value: 'anuragshukla87@yahoo.in' }],
   contact: [
     { name: 'Mr. Anurag Shukla', value: '9935443041' },
     { name: 'Mrs. Archna Shukla', value: '8948099022' },
